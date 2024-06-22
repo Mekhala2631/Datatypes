fac1c46e236d6a14c903730221767ebc7e63994b
Mekhala <mekhalapillutla8@gmail.com>
2024-06-22 18:42:12 +0530

    Add files via upload

---
+++ DataTypes/src/DataTypesDemo.java
@@ -0,0 +1,14 @@
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Character.MIN_VALUE);
+		System.out.println(Boolean.TRUE);
+
+	}
+
+}
